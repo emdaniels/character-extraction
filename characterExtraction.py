61615464c718881cb3b3276258ac519e05dcf021
Emily Daniels <daniele@algonquincollege.com>
2014-04-25 14:48:08 -0400

    Cleaned it up a bit; now PEP8 ok, kudos to Alex Pilon for enhancements and recommendations.

--- characterExtraction.py
+++ characterExtraction.py
@@ -1,74 +1,96 @@
-# Filename: characterExtraction.py
-# Author: Emily Daniels
-# Date: April 2014
-# Purpose: Extracts character names from a text file and performs analysis of text sentences containing the names.
-# Reference: See README.txt file for additional references
+""" Filename: characterExtraction.py
+Author: Emily Daniels
+Date: April 2014
+Purpose: Extracts character names from a text file and performs analysis of
+text sentences containing the names.
+Reference: See README.txt file for additional references """
 
-import nltk #Natural Language Toolkit
-from collections import Counter #used to count names
-import re #regular expression operations
-from collections import defaultdict #returns a new dictionary-like object
-import json #JavaScript Object Notation
+import json
+import nltk
+import re
+from collections import Counter
+from collections import defaultdict
+from nltk.corpus import stopwords
+from pattern.en import parse, Sentence, mood
+from pattern.db import csv
+from pattern.vector import Document, NB
 
-text = [] #holds the list of strings read from the text file
-entityNames = [] #holds the entity names
-chunkedSentences = () #holds the tagged and chunked sentences 
-majorCharacters = [] #holds the list of major characters
-sentenceList = [] #holds the list of extracted sentences
-characterSentences = defaultdict(list) #holds the dictionary of character sentences
-characterMoods = defaultdict(list) #holds the dictionary of character moods
-characterTones = defaultdict(list) #holds the dictionary of character tones
-sentenceAnalysis = defaultdict(list) #holds the dictionary of sentence analysis
+text = []
+entityNames = []
+chunkedSentences = ()
+majorCharacters = []
+sentenceList = []
+characterSentences = defaultdict(list)
+characterMoods = defaultdict(list)
+characterTones = defaultdict(list)
+sentenceAnalysis = defaultdict(list)
 
-#reads the text from a text file
+
+""" Reads the text from a text file. """
+
+
 def readText(text):
-    file = open("730.txt", "rb")
-    text = file.read()
-    file.close()
+    with open("730.txt", "rb") as f:
+        text = f.read()
     return text
 
 text = readText(text)
 
-#parses text into parts of speech tagged with pos labels
-#used for reference: https://gist.github.com/onyxfish/322906
+
+""" Parses text into parts of speech tagged with parts of speech labels.
+Used for reference: https://gist.github.com/onyxfish/322906 """
+
+
 def getChunkedSentences(chunkedSentences, text):
     sentences = nltk.sent_tokenize(text)
-    tokenizedSentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-    taggedSentences = [nltk.pos_tag(sentence) for sentence in tokenizedSentences]
+    tokenizedSentences = [nltk.word_tokenize(sentence)
+                          for sentence in sentences]
+    taggedSentences = [nltk.pos_tag(sentence)
+                       for sentence in tokenizedSentences]
     chunkedSentences = nltk.batch_ne_chunk(taggedSentences, binary=True)
     return chunkedSentences
 
 chunkedSentences = getChunkedSentences(chunkedSentences, text)
 
-#creates a local list to hold nodes of tree passed through, extracting named entities from the chunked sentences
-#used for reference: https://gist.github.com/onyxfish/322906
+
+""" Creates a local list to hold nodes of tree passed through, extracting named
+ entities from the chunked sentences.
+Used for reference: https://gist.github.com/onyxfish/322906 """
+
+
 def extractEntityNames(tree):
     entityNames = []
-    if hasattr(tree, 'node') and tree.node:
-        if tree.node == 'NE':
-            entityNames.append(' '.join([child[0] for child in tree]))
-        else:
-            for child in tree:
-                entityNames.extend(extractEntityNames(child))
+    try:
+        if hasattr(tree, 'node') and tree.node:
+            if tree.node == 'NE':
+                entityNames.append(' '.join([child[0] for child in tree]))
+            else:
+                for child in tree:
+                    entityNames.extend(extractEntityNames(child))
+    except AttributeError:
+        pass
     return entityNames
 
-#uses the global entity list, creating a new dictionary with the properties extended by the local list, without overwriting
-#used for reference: https://gist.github.com/onyxfish/322906
+
+""" Uses the global entity list, creating a new dictionary with the properties
+extended by the local list, without overwriting.
+used for reference: https://gist.github.com/onyxfish/322906 """
+
+
 def extendDict(chunkedSentences, entityNames):
     for tree in chunkedSentences:
         entityNames.extend(extractEntityNames(tree))
     return entityNames
 
 entityNames = extendDict(chunkedSentences, entityNames)
-print ("Done writing entity names.")
 
-#brings in stopwords and customstopwords to filter mismatches out
+
+""" Brings in stopwords and customstopwords to filter mismatches out. """
+
+
 def removeStopwords(entityNames):
-    customStopwords = []
-    file = open("customStopWords.txt", "rb")
-    customStopwords = file.read().split(', ')
-    file.close()
-    from nltk.corpus import stopwords
+    with open("customStopWords.txt", "rb") as f:
+        customStopwords = f.read().split(', ')
     for name in entityNames:
         if name in stopwords.words('english') or name in customStopwords:
             entityNames.remove(name)
@@ -76,23 +98,24 @@ def removeStopwords(entityNames):
 
 entityNames = removeStopwords(entityNames)
 
-#adds names to the major character list if they appear frequently
+
+""" Adds names to the major character list if they appear frequently. """
+
+
 def getMajorCharacters(entityNames, majorCharacters):
     for name in entityNames:
         if entityNames.count(name) > 10:
             majorCharacters.append(name)
     return majorCharacters
 
-majorCharacters = getMajorCharacters(entityNames, majorCharacters)
+majorCharacters = list(set(getMajorCharacters(entityNames, majorCharacters)))
 
-#removes duplicates
-majorCharacters = list(set(majorCharacters))
-#print majorCharacters
-print ("Done writing major characters.")
 
-#split sentences on .?! "" and not on abbreviations of titles
-#used for reference: http://stackoverflow.com/a/8466725
-def splitIntoSentences(text, sentenceList):
+""" Split sentences on .?! "" and not on abbreviations of titles.
+Used for reference: http://stackoverflow.com/a/8466725 """
+
+
+def splitIntoSentences(text):
     sentenceEnders = re.compile(r"""
     # Split sentences on whitespace between them.
     (?:               # Group for two positive lookbehinds.
@@ -107,68 +130,82 @@ def splitIntoSentences(text, sentenceList):
     (?<!  Prof\. )    # Don't end sentence on "Prof."
     (?<!  Sr\.   )    # Don't end sentence on "Sr."
     \s+               # Split on whitespace between sentences.
-    """, 
-    re.IGNORECASE | re.VERBOSE)
-    sentenceList = sentenceEnders.split(text)
-    return sentenceList
+    """, re.IGNORECASE | re.VERBOSE)
+    return sentenceEnders.split(text)
 
-sentenceList = splitIntoSentences(text, sentenceList)
+sentenceList = splitIntoSentences(text)
 
-#compares the list of sentences with the character names and returns sentences that include names
+
+""" Compares the list of sentences with the character names and returns
+sentences that include names. """
+
+
 def compareLists(sentenceList, majorCharacters, characterSentences):
     for sentence in sentenceList:
         for name in majorCharacters:
-            if re.search(r"\b(?=\w)%s\b(?!\w)" % name, sentence, re.IGNORECASE):
-                characterSentences[name].append(sentence)          
+            if re.search(r"\b(?=\w)%s\b(?!\w)" % name, sentence,
+                         re.IGNORECASE):
+                characterSentences[name].append(sentence)
     return characterSentences
 
-characterSentences = compareLists(sentenceList, majorCharacters, characterSentences)
+characterSentences = compareLists(sentenceList, majorCharacters,
+                                  characterSentences)
 
-#imports mood from pattern library, analyzes the sentence, and returns the grammatical mood
+
+""" Analyzes the sentence using grammatical mood module from pattern. """
+
+
 def extractMood(characterSentences, characterMoods):
-    from pattern.en import parse, Sentence
-    from pattern.en import mood
-    for key,value in characterSentences.iteritems():
+    for key, value in characterSentences.iteritems():
         for x in value:
-            x = parse(str(x), lemmata=True)
-            x = Sentence(x)
-            characterMoods[key].append(mood(x))
+            characterMoods[key].append(mood(Sentence
+                                            (parse(str(x), lemmata=True))))
     return characterMoods
 
 characterMoods = extractMood(characterSentences, characterMoods)
 
-#trains a Naive Bayes classifier object with the reviews.csv file, analyzes the sentence, and returns the tone
+
+""" Trains a Naive Bayes classifier object with the reviews.csv file, analyzes
+the sentence, and returns the tone. """
+
+
 def extractSentiment(characterSentences, characterTones):
-    from pattern.vector import Document, NB
-    from pattern.db import csv
     nb = NB()
     for review, rating in csv("reviews.csv"):
-        v = Document(review, type=int(rating), stopwords=True)
-        nb.train(v) 
+        nb.train(Document(review, type=int(rating), stopwords=True))
     for key, value in characterSentences.iteritems():
         for x in value:
             characterTones[key].append(nb.classify(str(x)))
     return characterTones
- 
+
 characterTones = extractSentiment(characterSentences, characterTones)
 
-#merges sentences, moods and tones together into one dictionary on each character
-sentenceAnalysis = dict([(k,[characterSentences[k],characterTones[k],characterMoods[k]]) for k in characterSentences])
 
-#writes the sentence analysis into a text file
-def writeAnalysis(sentenceAnalysis):            
-    file = open("sentenceAnalysis.txt", "wb")
-    for item in sentenceAnalysis.items():
-        file.write("%s:%s\n" % item)
-    file.close()
-    print ("Done writing sentenceAnalysis.")
+""" Merges sentences, moods and tones together into one dictionary on each
+ character. """
+
+
+sentenceAnalysis = dict([(k, [characterSentences[k],
+                              characterTones[k],
+                              characterMoods[k]]) for k in characterSentences])
+
+
+""" Writes the sentence analysis to a text file in the same directory. """
+
+
+def writeAnalysis(sentenceAnalysis):
+    with open("sentenceAnalysis.txt", "wb") as f:
+        for item in sentenceAnalysis.items():
+            f.write("%s:%s\n" % item)
 
 writeAnalysis(sentenceAnalysis)
 
-#writes the combined dictionaries to a JSON file
-def writeToJSON(sentenceAnalysis):    
-    with open("sentenceAnalysis.json", "wb") as outfile:
-        json.dump(sentenceAnalysis, outfile)
-    print ("Done writing to JSON.")
+
+""" Writes the sentence analysis to a JSON file in the same directory. """
+
+
+def writeToJSON(sentenceAnalysis):
+    with open("sentenceAnalysis.json", "wb") as f:
+        json.dump(sentenceAnalysis, f)
 
 writeToJSON(sentenceAnalysis)
