f21c1c3e16b7f1a5289e2b55905de77dc96ae17a
Emily Daniels <daniele@algonquincollege.com>
2014-04-28 11:07:46 -0400

    Added NLTK API version to readme, added fix for the use of either NLTK version 2 or 3, additional changes from Alex Pilon: using set comprehensions instead, sanitized regexes, and some style fixing.

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
-character-extraction
+CHARACTER EXTRACTION
 ====================
 
-Extracts character names from a text file and performs analysis of text sentences containing the names. See README.txt for more details.
+The purpose of this program is to extract the names of fictional characters 
+from a novel and analyze the sentences the characters appear in or are 
+referenced in within the text in order to build a profile containing data 
+specific to each character. It was created using the 32-bit version of Python 
+2.7 with the Natural Language Toolkit 2.0.4 and Pattern 2.6 libraries. 
+
+To change the book to be analyzed, add the book as a text file to the same 
+file directory as the program, change the name of the text file on line 27 
+of the file and rerun the program. You can also have the book file in a 
+different directory and reference the file path to the book instead.
+
+Author: Emily Daniels
+Date: April 2014
+Version: 1.0
+
+
+REFERENCES
+==========
+
+Oliver Twist
+------------
+
+This and all associated files of various formats will be found in:
+http://www.gutenberg.org/7/3/730/
+
+Produced by Peggy Gaugy and Leigh Little.  HTML version by Al Haines.
+
+This eBook is for the use of anyone anywhere at no cost and with
+almost no restrictions whatsoever.  You may copy it, give it away or
+re-use it under the terms of the Project Gutenberg License included
+with this eBook or online at www.gutenberg.net
+
+NLTK
+----
+
+Bird, Steven, Edward Loper and Ewan Klein (2009).
+Natural Language Processing with Python.  O'Reilly Media Inc.
+
+https://github.com/nltk/nltk/blob/develop/README.txt
+
+NLTK -- the Natural Language Toolkit -- is a suite of open source
+Python modules, data sets and tutorials supporting research and
+development in Natural Language Processing.
+
+NLTK source code is distributed under the Apache 2.0 License.
+NLTK documentation is distributed under the Creative Commons
+Attribution-Noncommercial-No Derivative Works 3.0 United States license.
+NLTK corpora are provided under the terms given in the README file
+for each corpus; all are redistributable, and available for non-commercial use.
+NLTK may be freely redistributed, subject to the provisions of these licenses.
+
+https://github.com/nltk/nltk/blob/develop/LICENSE.txt
+
+Pattern
+-------
+
+De Smedt, T., Daelemans, W. (2012). Pattern for Python. Journal of 
+Machine Learning Research, 13, 2031–2035.
+
+https://github.com/clips/pattern/blob/master/README.txt
+
+Pattern is a web mining module for Python. It has tools for data mining 
+(web services for Google, Twitter and Wikipedia, web crawler, HTML DOM parser), 
+natural language processing (part-of-speech taggers, n-gram search, sentiment 
+analysis, WordNet), machine learning (vector space model, clustering, 
+classification using KNN, SVM, Perceptron) and network analysis (graph 
+centrality and visualization). It is well documented and bundled with 50+ 
+examples and 350+ unit tests. The source code is licensed under BSD and 
+available from http://www.clips.ua.ac.be/pages/pattern.
+
+https://github.com/clips/pattern/blob/master/LICENSE.txt
--- characterExtraction.py
+++ characterExtraction.py
@@ -12,34 +12,26 @@ Reference: See README.txt file for additional references
 import json
 import nltk
 import re
-from collections import Counter, defaultdict
+
+from collections import defaultdict
 from nltk.corpus import stopwords
 from pattern.en import parse, Sentence, mood
 from pattern.db import csv
 from pattern.vector import Document, NB
 
-entityNames = []
-chunkedSentences = ()
-majorCharacters = []
-sentenceList = []
-characterSentences = defaultdict(list)
-characterMoods = defaultdict(list)
-characterTones = defaultdict(list)
-sentenceAnalysis = defaultdict(list)
-
-
 def readText():
-    """ Reads the text from a text file. """
+    """
+    Reads the text from a text file.
+    """
     with open("730.txt", "rb") as f:
         text = f.read().decode('utf-8-sig')
     return text
 
-text = readText()
 
-
-def getChunkedSentences(text):
+def chunkSentences(text):
     """
     Parses text into parts of speech tagged with parts of speech labels.
+
     Used for reference: https://gist.github.com/onyxfish/322906
     """
     sentences = nltk.sent_tokenize(text)
@@ -50,61 +42,67 @@ def getChunkedSentences(text):
     chunkedSentences = nltk.batch_ne_chunk(taggedSentences, binary=True)
     return chunkedSentences
 
-chunkedSentences = getChunkedSentences(text)
 
-
-def extractEntityNames(tree):
+def extractEntityNames(tree, _entityNames=None):
     """
     Creates a local list to hold nodes of tree passed through, extracting named
     entities from the chunked sentences.
+
     Used for reference: https://gist.github.com/onyxfish/322906
     """
-    entityNames = []
+    if _entityNames is None:
+        _entityNames = []
     try:
-        if hasattr(tree, 'node') and tree.node:
-            if tree.node == 'NE':
-                entityNames.append(' '.join([child[0] for child in tree]))
-            else:
-                for child in tree:
-                    entityNames.extend(extractEntityNames(child))
+        if nltk.__version__[0:2] == "2.":
+            label = tree.node
+        else:
+            label = tree.label()
     except AttributeError:
         pass
-    return entityNames
+    else:
+        if label == 'NE':
+            _entityNames.append(' '.join([child[0] for child in tree]))
+        else:
+            for child in tree:
+                extractEntityNames(child, _entityNames=_entityNames)
+    return _entityNames
 
 
-def extendDict(chunkedSentences, entityNames):
+def buildDict(chunkedSentences, _entityNames=None):
     """
     Uses the global entity list, creating a new dictionary with the properties
     extended by the local list, without overwriting.
+
     Used for reference: https://gist.github.com/onyxfish/322906
     """
+    if _entityNames is None:
+        _entityNames = []
+
     for tree in chunkedSentences:
-        entityNames.extend(extractEntityNames(tree))
+        extractEntityNames(tree, _entityNames=_entityNames)
 
-extendDict(chunkedSentences, entityNames)
+    return _entityNames
 
 
-def removeStopwords(entityNames):
-    """ Brings in stopwords and customstopwords to filter mismatches out. """
-    with open("customStopWords.txt", "rb") as f:
-        customStopwords = f.read().split(', ')
+def removeStopwords(entityNames, customStopWords=None):
+    """
+    Brings in stopwords and custom stopwords to filter mismatches out.
+    """
+    # Memoize custom stop words
+    if customStopWords is None:
+        with open("customStopWords.txt", "rb") as f:
+            customStopwords = f.read().split(', ')
+
     for name in entityNames:
         if name in stopwords.words('english') or name in customStopwords:
             entityNames.remove(name)
-    return entityNames
-
-entityNames = removeStopwords(entityNames)
 
 
 def getMajorCharacters(entityNames):
-    """ Adds names to the major character list if they appear frequently. """
-    majorCharacters = set()
-    for name in entityNames:
-        if entityNames.count(name) > 10:
-            majorCharacters.add(name)
-    return majorCharacters
-
-majorCharacters = getMajorCharacters(entityNames)
+    """
+    Adds names to the major character list if they appear frequently.
+    """
+    return {name for name in entityNames if entityNames.count(name) > 10}
 
 
 def splitIntoSentences(text):
@@ -120,7 +118,7 @@ def splitIntoSentences(text):
     )                 # End group of two positive lookbehinds.
     (?<!  Mr\.   )    # Don't end sentence on "Mr."
     (?<!  Mrs\.  )    # Don't end sentence on "Mrs."
-    (?<!  Ms\.  )     # Don't end sentence on "Ms."
+    (?<!  Ms\.   )    # Don't end sentence on "Ms."
     (?<!  Jr\.   )    # Don't end sentence on "Jr."
     (?<!  Dr\.   )    # Don't end sentence on "Dr."
     (?<!  Prof\. )    # Don't end sentence on "Prof."
@@ -129,42 +127,41 @@ def splitIntoSentences(text):
     """, re.IGNORECASE | re.VERBOSE)
     return sentenceEnders.split(text)
 
-sentenceList = splitIntoSentences(text)
 
-
-def compareLists(sentenceList, majorCharacters, characterSentences):
+def compareLists(sentenceList, majorCharacters):
     """
     Compares the list of sentences with the character names and returns
     sentences that include names.
     """
+    characterSentences = defaultdict(list)
     for sentence in sentenceList:
         for name in majorCharacters:
-            if re.search(r"\b(?=\w)%s\b(?!\w)" % name, sentence,
+            if re.search(r"\b(?=\w)%s\b(?!\w)" % re.escape(name),
+                         sentence,
                          re.IGNORECASE):
                 characterSentences[name].append(sentence)
     return characterSentences
 
-characterSentences = compareLists(sentenceList, majorCharacters,
-                                  characterSentences)
 
-
-def extractMood(characterSentences, characterMoods):
-    """ Analyzes the sentence using grammatical mood module from pattern. """
+def extractMood(characterSentences):
+    """
+    Analyzes the sentence using grammatical mood module from pattern.
+    """
+    characterMoods = defaultdict(list)
     for key, value in characterSentences.iteritems():
         for x in value:
             characterMoods[key].append(mood(Sentence(parse(str(x),
                                                            lemmata=True))))
     return characterMoods
 
-characterMoods = extractMood(characterSentences, characterMoods)
 
-
-def extractSentiment(characterSentences, characterTones):
+def extractSentiment(characterSentences):
     """
     Trains a Naive Bayes classifier object with the reviews.csv file, analyzes
     the sentence, and returns the tone.
     """
     nb = NB()
+    characterTones = defaultdict(list)
     for review, rating in csv("reviews.csv"):
         nb.train(Document(review, type=int(rating), stopwords=True))
     for key, value in characterSentences.iteritems():
@@ -172,28 +169,44 @@ def extractSentiment(characterSentences, characterTones):
             characterTones[key].append(nb.classify(str(x)))
     return characterTones
 
-characterTones = extractSentiment(characterSentences, characterTones)
-
-
-# Merges sentences, moods and tones together into one dictionary on each
-# character.
-sentenceAnalysis = dict([(k, [characterSentences[k],
-                              characterTones[k],
-                              characterMoods[k]]) for k in characterSentences])
-
 
 def writeAnalysis(sentenceAnalysis):
-    """ Writes the sentence analysis to a text file in the same directory. """
+    """
+    Writes the sentence analysis to a text file in the same directory.
+    """
     with open("sentenceAnalysis.txt", "wb") as f:
         for item in sentenceAnalysis.items():
             f.write("%s:%s\n" % item)
 
-writeAnalysis(sentenceAnalysis)
-
 
 def writeToJSON(sentenceAnalysis):
-    """ Writes the sentence analysis to a JSON file in the same directory. """
+    """
+    Writes the sentence analysis to a JSON file in the same directory.
+    """
     with open("sentenceAnalysis.json", "wb") as f:
         json.dump(sentenceAnalysis, f)
 
-writeToJSON(sentenceAnalysis)
+
+if __name__ == "__main__":
+    text = readText()
+
+    chunkedSentences = chunkSentences(text)
+    entityNames = buildDict(chunkedSentences)
+    removeStopwords(entityNames)
+    majorCharacters = getMajorCharacters(entityNames)
+    
+    sentenceList = splitIntoSentences(text)
+    characterSentences = compareLists(sentenceList, majorCharacters)
+    characterMoods = extractMood(characterSentences)
+    characterTones = extractSentiment(characterSentences)
+
+    # Merges sentences, moods and tones together into one dictionary on each
+    # character.
+    sentenceAnalysis = defaultdict(list,
+                                   [(k, [characterSentences[k],
+                                         characterTones[k],
+                                         characterMoods[k]])
+                                    for k in characterSentences])
+    
+    writeAnalysis(sentenceAnalysis)
+    writeToJSON(sentenceAnalysis)
